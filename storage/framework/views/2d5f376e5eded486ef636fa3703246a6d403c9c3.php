03daa4228895c650b1c51068d12c4856505bc768
Jaydeep Patel <jaydeep.patel@globalgenecorp.com>
2021-07-14 14:39:10 +0530

    Initial Commit

--- storage/framework/views/2d5f376e5eded486ef636fa3703246a6d403c9c3.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-	&copy; 2021 Tech Storiez. All Rights Reserved.
-</footer><?php /**PATH /var/www/html/demo/user-management/resources/views/admin/layouts/partials/footer.blade.php ENDPATH**/ ?>
